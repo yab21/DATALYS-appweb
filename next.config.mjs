ad530e1f10b99fc5b5d8adff84e03a4ca1c2aed9
yab21 <yablaiyablairubenvirgil@gmail.com>
2024-10-17 12:27:20 -1200

    new changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  output: "export",
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // output: "export",
 };
 
 export default nextConfig;
